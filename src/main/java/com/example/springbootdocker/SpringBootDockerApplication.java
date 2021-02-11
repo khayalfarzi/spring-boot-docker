699d8d6e9a2da5257125040cef3553978ea47573
khayalfarzi <xeyalferziyev9@gmail.com>
2021-02-11 11:51:52 +0300

    Initial commit

---
+++ src/main/java/com/example/springbootdocker/SpringBootDockerApplication.java
@@ -0,0 +1,25 @@
+package com.example.springbootdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
+
+    @GetMapping("/sec")
+    public String second() {
+        return "Hello Docker World second file";
+    }
+}
