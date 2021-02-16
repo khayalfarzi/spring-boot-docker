be911db65f3187912ab8fd62c97e18eddbe2da81
khayalfarzi <xeyalferziyev9@gmail.com>
2021-02-16 14:02:51 +0300

    test commit from cmd

--- src/test/java/com/example/springbootdocker/SpringBootDockerApplicationTests.java
+++ src/test/java/com/example/springbootdocker/SpringBootDockerApplicationTests.java
@@ -1,13 +1,52 @@
 package com.example.springbootdocker;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SpringBootDockerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void home() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+
+    @Test
+    void sec() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/sec")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+
+    @Test
+    void third() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/third")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+
+    @Test
+    void four() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/four")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+    }
 
 }
